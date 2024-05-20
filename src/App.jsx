992622c0bc81be01b46aa2450b7aa366e6255406
Colin MLG <colinmlg@MacBook-Air-de-Colin.local>
2024-05-20 21:14:29 +0200

    add sentry analytics to frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
+import * as Sentry from "@sentry/react";
+
 const App = () => {
   return (
     <main className="bg-black">
@@ -14,4 +15,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Sentry.withProfiler(App);
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,28 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://c1d3c8230b92b6f1dced8fb9ef197f0a@o4507289962086400.ingest.de.sentry.io/4507289964904528",
+  _experiments: {
+    metricsAggregator: true,
+  },
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
